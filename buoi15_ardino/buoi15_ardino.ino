ca37a6a5b1f1e0da97d19c744683066bb4bc3fc5
duong <damvanduong111@gmail.com>
2020-09-28 00:21:18 +0700

    tao log uart , ket noi wifi esp8266, tao socket tcp client , bat tat den form sever

--- buoi15_ardino/buoi15_ardino.ino
+++ buoi15_ardino/buoi15_ardino.ino
@@ -5,9 +5,11 @@
 HardwareSerial Log(UART0); // ham tao co doi so su dung uart0;
 ESP8266WiFiClass wifi;
 WiFiClient socket;
-
+uint8_t  DataOn[]="ON";
+uint8_t  DataOff[]="OFF";
+uint8_t  Data_rev[256]= {0};
 void setup() {
-  // put your setup code here, to run once:
+    pinMode(2,OUTPUT);
     Log.begin(9600);
     Log.printf("say hello form ESP8266\r\n");
     wifi.begin("mèo méo meo mèo meo !!!!!!!!!","bat3gmadung");
@@ -22,7 +24,42 @@ void setup() {
     char* msg = "hello ban oi\r\n";
     socket.write(msg,strlen(msg));
 }
+int sosanh(uint8_t *a,uint8_t *b)
+{
+  int n =0;
+  if(strlen((char *)a)>=strlen((char *)b))
+  {
+    n = strlen((char *)b);
+  }
+  else
+  {
+    n = strlen((char *)a);
+  }
+  int i =0;
+  for(i=0;i<n;i++)
+  {
+    if(a[i]!=b[i])
+    {
+      return 0;
+    }
+  }
+  return 1;
+}
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int DataSize =0;
+  if((DataSize = socket.available())>0)
+  {
+    socket.read(Data_rev, DataSize);
+    Log.printf("data from server : %s\r\n",Data_rev);
+    if(sosanh(Data_rev,DataOn)==1)
+    {
+      digitalWrite(2,0);
+    }
+    else if(sosanh(Data_rev,DataOff)==1)
+    {
+      digitalWrite(2,1);
+    }
+    memset((char *)Data_rev,0,strlen((char *)Data_rev));
+  }
+  
 }
